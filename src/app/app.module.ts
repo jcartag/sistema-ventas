c380f44fc95a2e273d15ddf72d6fef3f516fe7e8
DESKTOP-PK7782H\Andres Cartagena <AndresC>
2023-12-21 14:50:52 -0500

    se crean primeros componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { CoupensComponent } from './coupens/coupens.component';
+import { DeshboardComponent } from './deshboard/deshboard.component';
+import { MediaComponent } from './media/media.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { StatisComponent } from './statis/statis.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    CoupensComponent,
+    DeshboardComponent,
+    MediaComponent,
+    SidenavComponent,
+    StatisComponent
   ],
   imports: [
     BrowserModule
